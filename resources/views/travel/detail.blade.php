12a8d6e3579f1a9cebaa25e4d65d09aef30c659b
umaporn_do <umaporn_do@dtgo.com>
2021-04-19 10:37:59 +0700

    Finish template

--- resources/views/travel/detail.blade.php
+++ resources/views/travel/detail.blade.php
@@ -65,37 +65,102 @@
             <div class="container more-form-box">
                 <div class="row align-items-start">
                     <div class="col">
-                        <div class="card mb-3" style="max-width: 540px;">
+                        <div class="card m-1" style="border:none;">
                             <div class="row g-0">
-                                <div class="col-md-9">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <div class="col-md-8">
+                                    <div class="card-body p-0">
+                                        <h6 class="card-title">Card title</h6>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
                                         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                                     </div>
                                 </div>
-                                <div class="col-md-3">
-                                    <img src="{{ asset('images/200x200.jpg') }}" >
+                                <div class="col-md-4">
+                                    <img src="{{ asset('images/100x100.png') }}">
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="col">
-                        One of three columns
+                        <div class="card m-1" style="border:none;">
+                            <div class="row g-0">
+                                <div class="col-md-8">
+                                    <div class="card-body p-0">
+                                        <h6 class="card-title">Card title</h6>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <img src="{{ asset('images/100x100.png') }}">
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div class="col">
-                        One of three columns
+                        <div class="card m-1" style="border:none;">
+                            <div class="row g-0">
+                                <div class="col-md-8">
+                                    <div class="card-body p-0">
+                                        <h6 class="card-title">Card title</h6>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <img src="{{ asset('images/100x100.png') }}">
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="row align-items-center">
+                <div class="row align-items-center pt-2">
                     <div class="col">
-                        One of three columns
+                        <div class="card m-1" style="border:none;">
+                            <div class="row g-0">
+                                <div class="col-md-8">
+                                    <div class="card-body p-0">
+                                        <h6 class="card-title">Card title</h6>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <img src="{{ asset('images/100x100.png') }}">
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div class="col">
-                        One of three columns
+                        <div class="card m-1" style="border:none;">
+                            <div class="row g-0">
+                                <div class="col-md-8">
+                                    <div class="card-body p-0">
+                                        <h6 class="card-title">Card title</h6>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <img src="{{ asset('images/100x100.png') }}">
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div class="col">
-                        One of three columns
+                        <div class="card m-1" style="border:none;">
+                            <div class="row g-0">
+                                <div class="col-md-8">
+                                    <div class="card-body p-0">
+                                        <h6 class="card-title">Card title</h6>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <img src="{{ asset('images/100x100.png') }}">
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
