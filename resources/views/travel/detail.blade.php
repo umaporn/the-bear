c49da0435cf7f73590573ee768211b0124003839
umaporn_do <umaporn_do@dtgo.com>
2021-07-21 19:13:02 +0700

    Fixed bugs.

--- resources/views/travel/detail.blade.php
+++ resources/views/travel/detail.blade.php
@@ -1,28 +1,28 @@
 @extends('layouts.app')
 
 @section('page-title', __('travel.page_title.detail', [
-            'travel_title' => $contentDetail['data'][0]->title,
+            'travel_title' => isset($contentDetail['data'][0]->title) ? $contentDetail['data'][0]->title : '',
         ]))
 @section('page-description', __('travel.page_description.detail', [
-            'travel_description' => $contentDetail['data'][0]->description,
+            'travel_description' =>isset($contentDetail['data'][0]->description) ? $contentDetail['data'][0]->description : '' ,
         ]))
 @section('page-keyword', __('travel.page_keyword.detail',[
-            'travel_keyword' => $contentDetail['data'][0]->keyword,
+            'travel_keyword' => isset($contentDetail['data'][0]->keyword) ? $contentDetail['data'][0]->description : '',
         ]))
 
 @section('og-image', 'data:image/png;base64,'.$mainImage)
 @section('og-title', __('travel.og_title.detail', [
-            'travel_title' => $contentDetail['data'][0]->title,
+            'travel_title' => isset($contentDetail['data'][0]->title) ? $contentDetail['data'][0]->title : '',
         ] ))
 @section('og-description', __('travel.og_description.detail', [
-            'travel_description' => $contentDetail['data'][0]->description,
+            'travel_description' => isset( $contentDetail['data'][0]->description ) ? $contentDetail['data'][0]->description : '',
         ] ))
 @section('og-keyword', __('travel.og_keyword.detail',[
-            'travel_keyword' => $contentDetail['data'][0]->keyword,
+            'travel_keyword' => isset( $contentDetail['data'][0]->keyword ) ? $contentDetail['data'][0]->keyword : '',
         ]))
 @section('og-url', __('travel.og_url.detail',[
-            'travel_id' => $contentDetail['data'][0]->id,
-            'slug' => str_replace(" ", "-", $contentDetail['data'][0]->title)
+            'travel_id' => isset($contentDetail['data'][0]->id) ? $contentDetail['data'][0]->id : '',
+            'slug' => str_replace(" ", "-", isset( $contentDetail['data'][0]->title ) ? $contentDetail['data'][0]->title : '')
         ]) )
 
 @section('og-sitename', __('travel.page_title.index') )
@@ -50,7 +50,7 @@
         <div>
             <img src="data:image/png;base64,{{ $mainImage }}" class="w-100" alt="">
         </div>
-        <div class="container content-detail-660 pt-2">
+        <div class="container content-detail-660 pt-2" style="margin-top:30px">
             <div class="row pt-2">
                 <div class="col text-left">
                     BY {{ $contentDetail['data'][0]['Author'] ? $contentDetail['data'][0]['Author']->name : '' }}</div>
--- resources/views/travel/menu.blade.php
+++ resources/views/travel/menu.blade.php
@@ -1,28 +1,27 @@
 @extends('layouts.app')
-
 @section('page-title', __('travel.page_title.menu',[
-            'menu_title' => $contentList[0]['Menu']->menu_name ,
+            'menu_title' => isset( $contentList[0]['Menu']->menu_name )? $contentList[0]['Menu']->menu_name : '',
         ]))
 @section('page-description', __('travel.page_description.menu',[
-            'menu_title' => $contentList[0]['Menu']->menu_name ,
+            'menu_title' =>  isset( $contentList[0]['Menu']->menu_name )  ? $contentList[0]['Menu']->menu_name : '',
         ]))
 @section('page-keyword', __('travel.page_keyword.menu',[
-            'menu_title' => $contentList[0]['Menu']->menu_name ,
+            'menu_title' =>  isset( $contentList[0]['Menu']->menu_name )? $contentList[0]['Menu']->menu_name : '',
         ]))
 
 @section('og-image', asset(config('images.open_graph.default_image')))
 @section('og-title', __('travel.og_title.menu',[
-            'menu_title' => $contentList[0]['Menu']->menu_name,
+            'menu_title' =>  isset( $contentList[0]['Menu']->menu_name ) ? $contentList[0]['Menu']->menu_name : '',
         ]))
 @section('og-description', __('travel.og_description.menu',[
-            'menu_title' => $contentList[0]['Menu']->menu_name,
+            'menu_title' =>  isset( $contentList[0]['Menu']->menu_name )? $contentList[0]['Menu']->menu_name : '',
         ]))
 @section('og-keyword', __('travel.og_keyword.menu',[
-            'menu_title' => $contentList[0]['Menu']->menu_name,
+            'menu_title' =>  isset( $contentList[0]['Menu']->menu_name )? $contentList[0]['Menu']->menu_name : '',
         ]))
 @section('og-url', __('travel.og_url.menu',[
-            'menu_id' => $contentList[0]['Menu']->id,
-            'slug' => str_replace(' ', '-', $contentList[0]['Menu']->menu_name),
+            'menu_id' => isset($contentList[0]['Menu']->id )? $contentList[0]['Menu']->id : '',
+            'slug' => str_replace(' ', '-', isset( $contentList[0]['Menu']->menu_name ) ? $contentList[0]['Menu']->menu_name : ''),
         ]) )
 
 @section('content')
@@ -30,6 +29,7 @@
     </div>
     <div>
         <div class="container content-detail" style="margin-top:70px;">
+
             @if($contentList->total() !==  0)
                 <h1>{{ $contentList[0]['Menu']->menu_name }}</h1>
                 <div id="content-list-box">
