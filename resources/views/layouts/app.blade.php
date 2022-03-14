43ae4da6f5ee4f21d4c1cead5f1a42061a16b01d
umaporn_do <umaporn_do@dtgo.com>
2022-03-14 16:00:42 +0700

    Added http-equiv header.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="@yield('page-description')">
-
+    <meta http-equiv="Cache-Control" content="no-cache">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="expires" content="2 Sep 2030 05:00:00 GMT">
     @include('layouts.opengraph.facebook')
 
     <title>@yield('page-title')</title>
